0204c3c541761f953905bd00e347eb2ad1dd4399
june152 <sciencepyo@naver.com>
2023-02-20 16:46:33 +0900

    movie app detail work

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
 import axios from "axios"
+import styles from "./Detail.module.css"
 
 export default function Detail() {
     const [loading, setLoading] = useState(true)
@@ -18,16 +19,33 @@ export default function Detail() {
         setLoading(false)
     }, [])
     return (
-        <div>
-            <h1>Detail Infomation</h1>
-            <hr />
+        <div className={styles.detail}>
+            <div className={styles.detail__item}>
+            <h1 className={styles.detail__info}>Detail Infomation</h1>
             {loading ? <h2>Loading....</h2> : 
             <div>
-                <img src={info.large_cover_image} alt={info.title} />
-                <span>{info.title}</span>
+                <img src={info.large_cover_image} alt={info.title} className={styles.detail__cover}/>
             </div>
             }
             <h2><Link to={`/`}>Back</Link></h2>
+            </div>
+            {loading ? <h2>Loading....</h2> : 
+            <div className={styles.detail__div}>
+            <div>Title : {info.title}</div>
+            <div>Year : {info.year}</div>
+            <div>Rating : {info.rating} / 10.0</div>
+            <div>Runtime : {info.runtime} mins</div>
+            <div>
+                <ul className={styles.detail__genres}>
+                    genres : {info.genres && info.genres.map(g => <li key={g}>{g}</li>)}
+                </ul>
+            </div>
+            <div>
+                {info.description_full}
+            </div>
+        </div>
+            }
+            
         </div>
     )
 }
